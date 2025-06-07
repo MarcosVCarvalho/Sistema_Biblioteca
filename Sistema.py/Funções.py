f7f9213b0a66919205db98a109573dd615c7d58f
MarcosVCarvalho <carvalho.marcosvs@gmail.com>
2025-06-06 22:36:20 -0300

    versão 1.5
    Versão que envio a meu professor
    Essa será a versão final até eu aprender a deixar os dados de forma fixa, quando estiver com essa habilidades irei atualizar

--- Sistema.py/Funções.py
+++ Sistema.py/Funções.py
@@ -3,23 +3,24 @@ catalogo_livros = []
 catalogo_usuarios = []
 ids_existentes = []
 registros_emprestimos = []
+contador_id = 0
 
-def gerar_id_unico(minimo=0, maximo=9999, max_tentativas=10000):
-    global catalogo_livros
-    global catalogo_usuarios
-    global ids_existentes
-    for _ in range(max_tentativas):
-        novo_id = random.randint(minimo, maximo)
-        if novo_id not in ids_existentes:
+#Gera um id de 1 a 9999
+def gerar_id_unico( maximo=9999,):
+        global contador_id
+        contador_id += 1
+        novo_id = contador_id 
+        if novo_id not in ids_existentes and novo_id <= maximo:
             return novo_id
-        
+
+#Mande uma str e a função. Retorna ela em titulo    
 def criar_titulo(tex):
     print('='*80)
     print(f'{tex}'.center(80))
     print('='*80)
 
+#Mande o titulo,autor e ano de publicação. irá ser adicionado a um livro
 def Adicionarnovolivro(titulo, autor, ano_publicacao):
-    global catalogo_livros
     livro = {}
     livro.clear()
     
@@ -33,24 +34,24 @@ def Adicionarnovolivro(titulo, autor, ano_publicacao):
     catalogo_livros.append(livro.copy())
     return id
 
+#Lista todos os livros em catalogo_livros
 def Listartodososlivros():
-    global catalogo_livros
     print(f"{'ID':<5} {'Título':<30} {'Autor':<20} {'Ano':<10} {'Disponível':<12}")
     print("=" * 80)
     for livro in catalogo_livros:
         disponivel = 'Sim' if livro['disponivel'] == True else 'Não' 
         print(f"{livro['id']:<5} {livro['titulo']:<30} {livro['autor']:<20} {livro['ano_publicacao']:<10} {disponivel:<12}")
 
+#Mande um titulo. Retorna o resultado da busca
 def Buscarlivroportítulo(titulo_a_buscar):
-    global catalogo_livros
     resultado = []
     for livro in catalogo_livros:
         if titulo_a_buscar in livro['titulo'].upper() :
             resultado.append(livro)
     return resultado
 
+#Mande o nome e email. Será adicionado a um usuario
 def Adicionarnovousuário(nome_usuario, Email_usuario):
-    global catalogo_usuarios
     usuario = {}
     usuario.clear()
     
@@ -63,32 +64,31 @@ def Adicionarnovousuário(nome_usuario, Email_usuario):
 
     return id
 
+#Lista todos os usuarios em catalogo_usuarios
 def Listartodososusuários():
-    global catalogo_usuarios
     print(f"{'ID':<5}{'Nome do Usuario':<36} {'Email do Usuario':<36} ")
     print("=" * 80)
     for usuario in catalogo_usuarios:
         print(f"{usuario['id']:<5} {usuario['nome']:<36} {usuario['email']:<36}")
 
+#Mande o id do livro e se ele deve estar disponivel ou não
 def atualizar_disponibilidade_livro(id_livro, disponivel):
-    global catalogo_livros
     for livro in catalogo_livros:
         if id_livro == livro['id']:
             livro['disponivel'] = disponivel
             return
     print('Erro! Livro indisponível no momento'.center(80))
 
+#Mande o id de usuario e de livro. Verificará se eles existem no sistema
 def Verificador_para_emprestimo(id_usuario, id_livro):
-    global catalogo_livros
-    global catalogo_usuarios
 
     usuario_existe = any(usuario['id'] == id_usuario for usuario in catalogo_usuarios)
     livro_existe = any(livro['id'] == id_livro for livro in catalogo_livros)
     if usuario_existe and livro_existe == True:
         return True
     
+#Mande o id do livro, usuario. Irá realizar o emprestimo com a data de hoje
 def realizar_emprestimo(id_livro_desejado, id_usuario, data_emprestimo):
-    global registros_emprestimos
     emprestimo = {}
     emprestimo.clear()
 
@@ -103,8 +103,8 @@ def realizar_emprestimo(id_livro_desejado, id_usuario, data_emprestimo):
     registros_emprestimos.append(emprestimo.copy())
     return id
 
+#Mande o id do emprestimo e a data da devolução. Irá atualizar no sistema
 def devolver_livro(id_emprestimo, data_devolucao):
-    global registros_emprestimos
     for emprestimo in registros_emprestimos:
         if id_emprestimo == emprestimo['id']:
             emprestimo['data_devolucao'] = data_devolucao
@@ -112,44 +112,39 @@ def devolver_livro(id_emprestimo, data_devolucao):
             return
     print('Erro! ID não encontrado'.center(80))
 
+#Mostrar os emprestimos ativos
 def listar_emprestimos_ativos():
-    global registros_emprestimos
-    global catalogo_livros
-    global catalogo_usuarios
-    print(f"{'ID':<5}{'Nome do Usuario':<36}{'Titulo do Livro':<36} ")
-    print("=" * 80)
-    for emprestimo in registros_emprestimos:
-        for usuario in catalogo_usuarios:
-            for livro in catalogo_livros:
-                if usuario['id'] == emprestimo['id_usuario'] and emprestimo['data_devolucao'] == False and livro['id'] == emprestimo['id_livro']:
-                    nome_usuario = usuario['nome']
-                    id_emprestimo = emprestimo['id']
-                    titulo_livro = livro['titulo'] 
-                    try:
-                        print(f"{id_emprestimo:<5}{nome_usuario:<36}{titulo_livro:<36}")
-                    except:
-                        print('Sem emprestimos ativos')
+        print(f"{'ID':<5}{'Nome do Usuario':<36}{'Titulo do Livro':<36} ")
+        print("=" * 80)
+        for emprestimo in registros_emprestimos:
+            for usuario in catalogo_usuarios:
+                for livro in catalogo_livros:
+                    if usuario['id'] == emprestimo['id_usuario'] and emprestimo['data_devolucao'] == False and livro['id'] == emprestimo['id_livro']:
+                        try:
+                            print(f"{emprestimo['id']:<5}{usuario['nome']:<36}{livro['titulo']:<36}")
+                        except:
+                            print('Sem emprestimos ativos')
 
+#Mande o id de usuario. Mostrará seu historico de emprestimos
 def historico_emprestimos_usuario(id_usuario):
-    global registros_emprestimos
-    global catalogo_livros
-    global catalogo_usuarios
-    for usuario in catalogo_usuarios:
-        if id_usuario == usuario['id']:
-            nome = usuario['nome']
-    print(f"Emprestimos do {nome}".center(80))
-    print("=" * 80)
-    print(f"{'ID':<5} {'Titulo do Livro':<36} {'Estado do Emprestimo':<36} ")
-    print("=" * 80)
-    for emprestimo in registros_emprestimos:
-        if id_usuario == emprestimo['id_usuario']:
-            id_emprestimo = emprestimo['id']
-            if emprestimo['data_devolucao'] == False:
-                estado_emprestimo = 'Ativo'
-            else:
-                estado_emprestimo = 'Devolvido'
-            for livro in catalogo_livros:
-                if livro['id'] == emprestimo['id_livro']:
-                    titulo_livro = livro['titulo'] 
-        print(f"{id_emprestimo:<5} {titulo_livro:<36} {estado_emprestimo:<36}")    
-        
+    try:
+        for usuario in catalogo_usuarios:
+            if id_usuario == usuario['id']:
+                nome = usuario['nome']
+        print(f"Emprestimos do {nome}".center(80))
+        print("=" * 80)
+        print(f"{'ID':<5} {'Titulo do Livro':<36} {'Estado do Emprestimo':<36} ")
+        print("=" * 80)
+        for emprestimo in registros_emprestimos:
+            if id_usuario == emprestimo['id_usuario']:
+                id_emprestimo = emprestimo['id']
+                if emprestimo['data_devolucao'] == False:
+                    estado_emprestimo = 'Ativo'
+                else:
+                    estado_emprestimo = 'Devolvido'
+                for livro in catalogo_livros:
+                    if livro['id'] == emprestimo['id_livro']:
+                        titulo_livro = livro['titulo'] 
+            print(f"{id_emprestimo:<5} {titulo_livro:<36} {estado_emprestimo:<36}")    
+    except:
+        print('Erro! Tente Novamente')
--- Sistema.py/menu.py
+++ Sistema.py/menu.py
@@ -1,6 +1,7 @@
 import Funções
 import datetime 
 
+#Menu
 while True:
     print('='*80)
     print('Menu Principal'.center(80))
@@ -17,6 +18,7 @@ while True:
     print('10:Sair do sistema')
     print('='*80)
 
+    #Validação de erro
     try:
         op = int(input('Digite sua opção: '))
         if op in range(11):
@@ -31,30 +33,36 @@ while True:
     
     if opcao_Valida is not None:
         if opcao_Valida == 1:
-            Funções.criar_titulo('Adicionar novo livro')
-            titulo = input('titulo do livro: ')
-            autor = input('Autor do livro: ')
-            ano_publicacao = int(input('Ano de publicação: '))
-            id_livro = Funções.Adicionarnovolivro(titulo, autor, ano_publicacao)
-            print(f'Livro adicionado com sucesso - ID:{id_livro}')
+            try:
+                Funções.criar_titulo('Adicionar novo livro')
+                titulo = input('titulo do livro: ')
+                autor = input('Autor do livro: ')
+                ano_publicacao = int(input('Ano de publicação: '))
+                id_livro = Funções.Adicionarnovolivro(titulo, autor, ano_publicacao)
+                print(f'Livro adicionado com sucesso - ID:{id_livro}')
+            except:
+                print('ERRO! Digite novamente')
 
         elif opcao_Valida == 2:
             Funções.criar_titulo('Listar todos os livros')
             Funções.Listartodososlivros()
             
         elif opcao_Valida == 3:
-            Funções.criar_titulo('Buscar livro por título')
-            titulo_a_buscar = str(input('Qual o título do livro: ')).upper()
-            livro_buscado = Funções.Buscarlivroportítulo(titulo_a_buscar)
-            if livro_buscado:
-                print(f'livros encontrados: {len(livro_buscado)}'.center(80))
-                print("=" * 80)
-                print(f"{'ID':<5} {'Título':<30} {'Autor':<20} {'Ano':<10} {'Disponível':<12}")
-                print("=" * 80)
-                for livro in livro_buscado:
-                    print(f"{livro['id']:<5} {livro['titulo']:<30} {livro['autor']:<20} {livro['ano_publicacao']:<10} {livro['disponivel']:<12}")
-            else:
-                Funções.criar_titulo('Livro não encontrado')
+            try:
+                Funções.criar_titulo('Buscar livro por título')
+                titulo_a_buscar = str(input('Qual o título do livro: ')).upper()
+                livro_buscado = Funções.Buscarlivroportítulo(titulo_a_buscar)
+                if livro_buscado:
+                    print(f'livros encontrados: {len(livro_buscado)}'.center(80))
+                    print("=" * 80)
+                    print(f"{'ID':<5} {'Título':<30} {'Autor':<20} {'Ano':<10} {'Disponível':<12}")
+                    print("=" * 80)
+                    for livro in livro_buscado:
+                        print(f"{livro['id']:<5} {livro['titulo']:<30} {livro['autor']:<20} {livro['ano_publicacao']:<10} {livro['disponivel']:<12}")
+                else:
+                    Funções.criar_titulo('Livro não encontrado')
+            except:
+                print('ERRO! Digite novamente')
 
         elif opcao_Valida == 4:
             Funções.criar_titulo('Adicionar novo usuário')
@@ -68,32 +76,41 @@ while True:
             Funções.Listartodososusuários()
         
         elif opcao_Valida == 6:
-            Funções.criar_titulo('Realizar um empréstimo')
-            id_usuario = int(input('Digite o id do usuario: '))
-            id_livro_desejado = int(input('Digite o id do livro desejado: '))
-            verificador = Funções.Verificador_para_emprestimo(id_usuario,id_livro_desejado)
-            if verificador == True:
-                data_emprestimo = datetime.datetime.now()
-                id_emprestimo = Funções.realizar_emprestimo(id_livro_desejado, id_usuario, data_emprestimo)
-                print(f'Emprestimo realizado com sucesso - ID:{id_emprestimo}')
-            else:
-                print('Erro! id de usuario ou livro inexistente')
+            try:
+                Funções.criar_titulo('Realizar um empréstimo')
+                id_usuario = int(input('Digite o id do usuario: '))
+                id_livro_desejado = int(input('Digite o id do livro desejado: '))
+                verificador = Funções.Verificador_para_emprestimo(id_usuario,id_livro_desejado)
+                if verificador == True:
+                    data_emprestimo = datetime.datetime.now()
+                    id_emprestimo = Funções.realizar_emprestimo(id_livro_desejado, id_usuario, data_emprestimo)
+                    print(f'Emprestimo realizado com sucesso - ID:{id_emprestimo}')
+                else:
+                    print('Erro! id de usuario ou livro inexistente')
+            except:
+                print('ERRO! Digite novamente')
 
         elif opcao_Valida == 7:
-            Funções.criar_titulo('Devolver um livro')
-            id_emprestimo = int(input('Digite o id do emprestimo: '))
-            data_devolucao = datetime.datetime.now()
-            Funções.devolver_livro(id_emprestimo, data_devolucao)
-            Funções.criar_titulo('Devolução realizada com sucesso')
+            try:
+                Funções.criar_titulo('Devolver um livro')
+                id_emprestimo = int(input('Digite o id do emprestimo: '))
+                data_devolucao = datetime.datetime.now()
+                Funções.devolver_livro(id_emprestimo, data_devolucao)
+                Funções.criar_titulo('Devolução realizada com sucesso')
+            except:
+                print('ERRO! Digite novamente')
 
         elif opcao_Valida == 8:
             Funções.criar_titulo('Listar empréstimos ativos')
             Funções.listar_emprestimos_ativos()
           
         elif opcao_Valida == 9:
-            Funções.criar_titulo('Visualizar histórico de empréstimos de um usuário')
-            id_usuario = int(input('Digite o id do usuario: '))
-            Funções.historico_emprestimos_usuario(id_usuario)
+            try:
+                Funções.criar_titulo('Visualizar histórico de empréstimos de um usuário')
+                id_usuario = int(input('Digite o id do usuario: '))
+                Funções.historico_emprestimos_usuario(id_usuario)
+            except:
+                print('ERRO! Digite novamente')
            
         elif opcao_Valida == 10:
             Funções.criar_titulo('Fechando Menu - Volte Sempre')
